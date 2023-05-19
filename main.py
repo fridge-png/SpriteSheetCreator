3431fd56c42091c26d683e30bb99f384f20ff1a2
Youssef Darwich <58274928+fridge-png@users.noreply.github.com>
2023-05-20 00:38:35 +0200

    Added margin and some fixes

--- main.py
+++ main.py
@@ -2,24 +2,23 @@ from PIL import Image
 import os
 from enum import Enum
     
-# This script layers png images on top of each other and creates a final layered png file
+# This script create a sprite sheet from a set of images
 # The order of the image must be appended to the end of its name like so: SampleImage_1.png
-# An image with a lower order will apear behind an image with a higher order
 
 #========================================
 # FIELDS TO CHANGE 
 # path of the directory that contains your images 
 imageDir = "SampleImages/"
 # path of the directory that you want your final image to be saved in 
-saveDir = "SavedImages/"
+saveDir = "SavedImage/"
 # what you want your final image to be called
 finalImageName = "FinalImage"
 # x,y dimensions of your images (all images will be cropped to this size)
 imageDim = [50,50]
-# rowNum = 2
+# number of columns in each row
 colNum = 10
-# set to true if you want your cropped images to be saved in the croppedDir set above
-saveCroppedImages = True
+# margin between each image on the spritesheet
+imageMargin = 0
 #========================================
 
 # checks if the directories exist
@@ -36,20 +35,20 @@ def getNum(img):
     except ValueError:
         return float('inf')
 
-# crops image to the imageDim values with the offsetCrop offset
+# crops image to the imageDim values
 def cropImage(image):
     #left top right bottom
     image = image.crop((0,0,imageDim[0],imageDim[1]))
     return image
 
-# uses the PIL library to layer images and saves to the save directory
+# uses the PIL library to paste the images and saves to the save directory
 def layerImages():
     images = os.listdir(imageDir)
     images.sort(key=getNum)
     try:
-        spriteSheet = Image.new("RGBA",(colNum*imageDim[0],(int(len(images)/colNum))*imageDim[1]))
+        spriteSheet = Image.new("RGBA",(colNum*(imageDim[0]+imageMargin),(int(len(images)/colNum))*(imageDim[1]+imageMargin)))
     except Exception as e:
-        print("Error while reading images.")
+        print("Error while creating image.")
         print(e)
         return
     for i,image in enumerate(images):
@@ -59,14 +58,9 @@ def layerImages():
             else:
                 try:
                     img = Image.open(imageDir + image)
-                    # img = cropImage(img)
-                    # if(saveCroppedImages):
-                    #     img2.save(croppedDir + "Cropped_" + image)
-                    # if(i%colNum == 0):
+                    img = cropImage(img)
                     print (i+1)
-                    spriteSheet.paste(img, ((((i)%colNum)*imageDim[0],int((i)/colNum)*imageDim[1])),img)
-                    # else:
-                        # img1.paste(img2, (0,0), mask = img2)
+                    spriteSheet.paste(img, ((((i)%colNum)*(imageDim[0]+imageMargin),int((i)/colNum)*(imageDim[1]+imageMargin))),img)
                 except Exception as e:
                     print("Error while reading images.")
                     print(e)
