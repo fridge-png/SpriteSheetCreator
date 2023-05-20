fc5e2d6168945f0e01f30305563d5b4b778e4e5e
Youssef Darwich <58274928+fridge-png@users.noreply.github.com>
2023-05-20 10:47:37 +0200

    Added some fixes

--- SampleImages/README.md
+++
@@ -1,6 +0,0 @@
-#Sprite Sheet Creator
-Creates sprite sheet image from a sequence of images
-
-
-## Final Spritesheet
-![Image not found](/SavedImage/FinalImage.png?raw=true "Final Spritesheet")
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from PIL import Image
 import os
-from enum import Enum
     
 # This script create a sprite sheet from a set of images
 # The order of the image must be appended to the end of its name like so: SampleImage_1.png
@@ -13,12 +12,15 @@ imageDir = "SampleImages/"
 saveDir = "SavedImage/"
 # what you want your final image to be called
 finalImageName = "FinalImage"
-# x,y dimensions of your images (all images will be cropped to this size)
-imageDim = [50,50]
-# number of columns in each row
-colNum = 10
+# x,y dimensions of your images (all images will be resized to this size)
+imageDim = [40,40]
+# set to true if the spritesheet images should go horizontally
+byCol = True
+# number of columns in each row / num of rows in each column
+colRowNum = 5
 # margin between each image on the spritesheet
-imageMargin = 0
+imageXMargin = 0
+imageYMargin = 0
 #========================================
 
 # checks if the directories exist
@@ -36,9 +38,9 @@ def getNum(img):
         return float('inf')
 
 # crops image to the imageDim values
-def cropImage(image):
+def resizeImage(image):
     #left top right bottom
-    image = image.crop((0,0,imageDim[0],imageDim[1]))
+    image = image.resize((imageDim[0],imageDim[1]))
     return image
 
 # uses the PIL library to paste the images and saves to the save directory
@@ -46,7 +48,10 @@ def layerImages():
     images = os.listdir(imageDir)
     images.sort(key=getNum)
     try:
-        spriteSheet = Image.new("RGBA",(colNum*(imageDim[0]+imageMargin),(int(len(images)/colNum))*(imageDim[1]+imageMargin)))
+        if(byCol):
+            spriteSheet = Image.new("RGBA",(colRowNum*(imageDim[0]+imageXMargin),(int(len(images)/colRowNum))*(imageDim[1]+imageYMargin)))
+        else:
+            spriteSheet = Image.new("RGBA",((int(len(images)/colRowNum))*(imageDim[1]+imageXMargin),colRowNum*(imageDim[0]+imageYMargin)))
     except Exception as e:
         print("Error while creating image.")
         print(e)
@@ -58,9 +63,11 @@ def layerImages():
             else:
                 try:
                     img = Image.open(imageDir + image)
-                    img = cropImage(img)
-                    print (i+1)
-                    spriteSheet.paste(img, ((((i)%colNum)*(imageDim[0]+imageMargin),int((i)/colNum)*(imageDim[1]+imageMargin))),img)
+                    img = resizeImage(img)
+                    if(byCol):
+                        spriteSheet.paste(img, ((((i)%colRowNum)*(imageDim[0]+imageXMargin),int((i)/colRowNum)*(imageDim[1]+imageYMargin))),img)
+                    else:
+                        spriteSheet.paste(img, (((int((i)/colRowNum)*(imageDim[1]+imageXMargin),(i%colRowNum)*(imageDim[0]+imageYMargin)))),img)
                 except Exception as e:
                     print("Error while reading images.")
                     print(e)
